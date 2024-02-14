e5c945d3bf5f8d916ae4f01ed5527d6783ae7f01
Shahadat <shahadat.akash007@gmail.com>
2024-02-15 00:33:30 +0600

    build files

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const path = require('path');
+const cors = require('cors')
 
 dotenv.config({path: './config/config.env'})
 connectDB()
 const transactions = require('./routes/transactions')
 
 const app = express()
+
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'development') {
